6897961ba64698de0a4db6e4eeadfe82f7b487bb
Mohammed alotaibi <muhammedalotaibi13@gmail.com>
2018-01-27 23:43:30 +0300

    config firebase with app

---
+++ ChattingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChattingApp
+//
+//  Created by محمد عايض العتيبي on 5/10/1439 AH.
+//  Copyright © 1439 code schoole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
